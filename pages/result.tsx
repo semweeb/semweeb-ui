bd50e3c946b63960a50cf8a8cf98f033bd5f6901
Muhammad Asyraf <muhammad.asyraf@ui.ac.id>
2022-12-21 21:23:24 +0700

    feat: details info box
    Co-authored-by: raissatito <raissatito@users.noreply.github.com>
    Co-authored-by: Julius Raka <rakazet@users.noreply.github.com>
    Co-authored-by: Adrian Ardizza <adrian.ardizza@gmail.com>

--- pages/result.tsx
+++ pages/result.tsx
@@ -1,16 +1,31 @@
 import { useRouter } from "next/router"
+import { useEffect, useState } from 'react'
+import dynamic from "next/dynamic"
+
+const ReactPlayer = dynamic(() => import('react-player'), {
+  ssr: false
+})
 
 export default function Result() {
   const router = useRouter()
 
   const { id } = router.query
+  const [data, setData] = useState<any>({})
+
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/details?id=${id}`)
+      .then((res) => res.json())
+      .then((res) => setData(res))
+  }, [id])
+
 
   return (
     <div className="w-full min-h-screen h-full flex justify-center py-20">
       <div className="w-full h-full grid grid-cols-12 gap-12 max-w-7xl mx-16">
         <div className="h-full w-full space-y-4 col-span-3">
           <div className="rounded-lg w-full aspect-[75/106] overflow-hidden">
-            <img className="w-full h-full object-cover" src="/dekdekpe.png" />
+            <img className="w-full h-full object-cover" src={data?.main_picture?.value} />
           </div>
           <div>
             <h1 className="font-bold">
@@ -23,11 +38,11 @@ export default function Result() {
                   region code -> full country name aja (gua urus nanti -adrian) */}
               <p>
                 <span className="font-bold">Japanese: </span>
-                <span>りゅうおうのおしごと！</span>
+                <span>{data?.title_japanese?.value}</span>
               </p>
               <p>
                 <span className="font-bold">English: </span>
-                <span>The Ryuo&apos;s Work is Never Done!</span>
+                <span>{data?.title_english?.value}</span>
               </p>
             </div>
           </div>
@@ -40,50 +55,71 @@ export default function Result() {
               <p>
                 {/* Ini nanti pake type yang udah ada di csv kemarin aja */}
                 <span className="font-bold">Type: </span>
-                <span>TV Series</span>
+                <span>{data?.type_name?.value}</span>
               </p>
               <p>
                 <span className="font-bold">Source Material: </span>
-                <span>Original</span>
+                <span>{data?.source_name?.value}</span>
               </p>
               <p>
                 <span className="font-bold">Episodes: </span>
-                <span>12</span>
+                <span>{data?.episodes?.value}</span>
               </p>
               <p>
                 <span className="font-bold">Status: </span>
-                <span>Finished</span>
+                <span>{data?.status_name?.value}</span>
               </p>
               <p>
                 <span className="font-bold">Aired: </span>
-                <span>Jan 8, 2018 to Mar 26, 2018</span>
+                <span>{data?.start_date?.value} to {data?.end_date?.value}</span>
               </p>
               <p>
                 <span className="font-bold">Studios: </span>
-                <span>Project No. 9</span>
+                <span>{data?.studios?.value}</span>
               </p>
             </div>
           </div>
         </div>
         <div className="w-full col-span-9">
           <div>
-            <h1 className="text-3xl">DekDekPe: Programming Basics</h1>
-            <h2 className="text-xl text-gray-600">Action, Adventure, Psychological</h2>
+            <h1 className="text-3xl">{data?.title?.value}</h1>
+            <h2 className="text-xl text-gray-600"> {data?.genres?.value}</h2>
           </div>
 
           <hr className="my-4" />
 
-          <h3 className="text-2xl mb-2 font-semibold">Synopsis</h3>
-          <p className="text-lg">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat odio, sodales a ipsum ut, pellentesque accumsan mi. Ut nulla magna, consectetur eu accumsan cursus, viverra id lacus. Cras fringilla nibh sit amet elit efficitur pharetra. Nullam quis elit volutpat lectus dapibus pulvinar et in ex. Integer at erat non quam malesuada sodales vitae vitae arcu. Proin egestas leo et justo rhoncus dignissim. Integer ac dui id odio ornare tincidunt non eget nunc. Phasellus erat dolor, sollicitudin vitae enim tristique, tristique auctor enim. Nulla et arcu ornare, venenatis neque eget, sagittis turpis. Aliquam non ante nec metus posuere placerat.
-          </p>
+          {data?.synopsis && (
+            <>
+              <h3 className="text-2xl mb-2 font-semibold">Synopsis</h3>
+              <p className="text-lg">
+                {data?.synopsis?.value}
+              </p>
+              <hr className="my-4" />
+            </>
+          )}
 
-          <hr className="my-4" />
 
-          <h3 className="text-2xl mb-2 font-semibold">Background</h3>
-          <p className="text-lg">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat odio, sodales a ipsum ut, pellentesque accumsan mi. Ut nulla magna, consectetur eu accumsan cursus, viverra id lacus.
-          </p>
+          
+          {data?.background && (
+            <>
+              <h3 className="text-2xl mb-2 font-semibold">Background</h3>
+              <p className="text-lg">
+                {data?.background?.value}
+              </p>
+              <hr className="my-4" />
+            </>
+          )}
+          
+          {data?.trailer_url && (
+            <>
+              <h3 className="text-2xl mb-2 font-semibold">Trailer</h3>
+              <div>
+                <ReactPlayer url={data?.trailer_url?.value} />
+              </div>
+              <hr className="my-4" />
+            </>
+          )}
+
         </div>
       </div>
     </div>
